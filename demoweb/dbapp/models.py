91e7b598f0c2f660a70503e0bdd4ecf9bfd92f84
caihui <cai.hui@zte.com.cn>
2018-06-19 20:02:25 +0800

    add a table & register admin web

--- demoweb/dbapp/models.py
+++ demoweb/dbapp/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Demoapp(models.Model):
+    demo_id = models.CharField(max_length=10)
+    ceate_time = models.DateTimeField(auto_now=True)
+    demo_content = models.CharField(max_length=50)
+   # def __str__(self):
+   #     return self.name
+
+
+        
+    
